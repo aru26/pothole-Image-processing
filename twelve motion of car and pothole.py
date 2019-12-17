1e3b6e2f808ad7814e45ba3f1354edb46a15cd02
aru26 <50775209+aru26@users.noreply.github.com>
2019-12-17 05:56:44 +0530

    Add files via upload

---
+++ twelve motion of car and pothole.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+cap=cv2.VideoCapture('pothole.mp4')
+fgbg=cv2.createBackgroundSubtractorMOG2()
+
+while True:
+    ret,frame=cap.read()
+    fgmask=fgbg.apply(frame)
+
+    cv2.imshow('original', frame)
+    cv2.imshow('fg ',fgmask)
+
+    k=cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
